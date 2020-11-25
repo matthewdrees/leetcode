7bb0e1a80a40f74be14e0e7ae13063fe94e146e1
Matt Drees <matt@drees.me>
2020-11-24 16:04:41 -0800

    wip.

--- leetcode/py/p4-median-of-two-sorted-arrays.py
+++ leetcode/py/p4-median-of-two-sorted-arrays.py
@@ -11,6 +11,41 @@ import statistics
 from typing import List
 
 
+class BisectIndex:
+    """Track an index for bisecting a collection with length n."""
+
+    def __init__(self, low, high):
+        self.right = high
+        self.left = low
+        self.index = (high + low) // 2
+
+    def get_index(self):
+        return self.index
+
+    def go_left(self):
+        if self.index <= self.left:
+            return False
+        self.right = self.index
+        self.index -= (self.index - self.left + 1) // 2
+        return True
+
+    def go_right(self):
+        if self.index >= self.right:
+            return False
+        self.left = self.index
+        self.index += (self.right - self.index + 1) // 2
+        return True
+
+
+def calculate_bounds(nums1, nums2):
+    len1 = len(nums1)
+    len2 = len(nums2)
+    bound = (len1 - len2) // 2
+    if bound > 0:
+        return bound, len1 - bound - 1
+    return 0, len1 - 1
+
+
 class Solution:
     def medianOfSortedArray(self, nums: List[int]) -> float:
         if len(nums) % 2 == 0:
@@ -19,32 +54,66 @@ class Solution:
         else:
             return float(nums[len(nums) // 2])
 
+    def findMedianSortedArraysOdd(self, nums1: List[int], nums2: List[int]) -> float:
+
+        # Put the odd list first.
+        if len(nums1) % 2 == 0:
+            nums1, nums2 = nums2, nums1
+        assert len(nums1) % 2 == 1
+
+        low, high = calculate_bounds(nums1, nums2)
+        bi1 = BisectIndex(low, high)
+        i2 = (len(nums2) - 1) // 2
+        i2_low, i2_high = calculate_bounds(nums2, nums1)
+        while True:
+            i1 = bi1.get_index()
+            if i2 < 0 or i2 >= len(nums2):
+                return nums1[i1]
+            if nums1[i1] < nums2[i2]:
+                i2_high = i2
+                if bi1.go_right():
+                    i2 -= i1 - bi1.get_index()
+                    continue
+                else:
+                    # number is in nums2
+                    break
+
+            # if i2 is last index in nums2:
+            if i2 + 1 >= len(nums2):
+                if nums1[i1] < nums2[i2]:
+                    return nums2[i2]
+                else:
+                    return nums1[i1]
+
+            if nums1[i1] > nums2[i2 + 1]:
+                i2_low = i2 + 1
+                if bi1.go_left():
+                    i2 += bi1.get_index() - i1
+                    continue
+                else:
+                    break
+
+            return nums1[i1]
+
+        return nums2[(i2_high + i2_low) // 2]
+
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
 
-        # One or both are empty
         if len(nums1) == 0 and len(nums2) == 0:
             return None
-        if len(nums1) == 0:
-            return self.medianOfSortedArray(nums2)
-        if len(nums2) == 0:
-            return self.medianOfSortedArray(nums1)
 
-        # Odd number of elements
-
-        # Start off in middle of each.
-
-        # Whichever number is bigger, set ranges, from that number to end.
+        if (len(nums1) + len(nums2)) % 2 == 1:
+            return self.findMedianSortedArraysOdd(nums1, nums2)
+        return 1.0
 
 
 def test_all_combinations(arr):
     """Test all combinations, making 2 arrays out of arr."""
 
+    passed = True
     expected = statistics.median(arr)
-    for len_arr_1 in range(len(arr)):
-        len_arr_2 = len(arr) - len_arr_1
-        for l1_indexes in itertools.combinations_with_replacement(
-            range(len_arr_1), len_arr_1
-        ):
+    for len_arr_1 in range(len(arr) + 1):
+        for l1_indexes in itertools.combinations(range(len(arr)), len_arr_1):
             l1 = []
             l2 = []
             for x in range(len(arr)):
@@ -52,9 +121,21 @@ def test_all_combinations(arr):
                     l1.append(arr[x])
                 else:
                     l2.append(arr[x])
-            print("-----")
-            print(l1)
-            print(l2)
+            s = Solution()
+            actual = s.findMedianSortedArrays(l1, l2)
+            if expected is None:
+                passed = actual is None
+            else:
+                if actual is None:
+                    passed = False
+                else:
+                    passed = math.isclose(expected, actual, abs_tol=0.01)
+            if not passed:
+                print(
+                    f"FAIL. findMedianSortedArrays({l1}, {l2}), exp: {expected}, ans: {actual}"
+                )
+
+    return passed
 
 
 def test_findMedianSortedArrays():
@@ -86,5 +167,7 @@ def test_findMedianSortedArrays():
 
 
 if __name__ == "__main__":
-    test_findMedianSortedArrays()
-    test_all_combinations([1, 2, 3])
+    # test_findMedianSortedArrays()
+    s = Solution()
+    print(s.findMedianSortedArrays([1, 3], [2, 4, 5]))
+    test_all_combinations([1, 2, 3, 4, 5])
